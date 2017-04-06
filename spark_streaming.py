66c4842cc0977732df98d0bfbc99de0742eb1a6e
Hari om Singh <hariomsingh2007@gmail.com>
2017-04-06 22:15:04 +0530

    Create spark_streaming.py
    spark-submit --master local[2] --conf "spark.dynamicAllocation.enabled=false" \
    --jars  /home/cloudera/Desktop/kafka/spark-streaming-kafka-assembly_2.10-1.3.0.jar \
    /home/cloudera/Desktop/kafka/spark_streaming_kafka.py localhost:2181 data1

---
+++ spark_streaming.py
@@ -0,0 +1,26 @@
+'''@Author : Hari om Singh'''
+
+from __future__ import print_function
+
+import sys
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
+        exit(-1)
+
+    sc = SparkContext(appName="PythonStreamingKafkaWordCount")
+    ssc = StreamingContext(sc, 5)
+    sc.setLogLevel("ERROR")
+
+    zkQuorum, topic = sys.argv[1:]
+    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    lines = kvs.map(lambda x: x[1])
+    lines.pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
